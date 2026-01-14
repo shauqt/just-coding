3e31bafdd3a4b3e0abe072aee226fc080808ad4f
Shauhard Bhandari <193943014+shauqt@users.noreply.github.com>
2026-01-13 20:57:25 -0500

    fixed conversion

--- decimal_to_base_n_converter.py
+++ decimal_to_base_n_converter.py
@@ -8,9 +8,7 @@ base = int(input('What base do you want the number respresented?: ').strip())
 # Main function will call decimal_convert function and display converted number
 def main():
     z = decimal_convert(number, base)
-        
-    for i in range(len(z)):
-        print(z[i], sep = None ,end = '')
+    print(z)
 
 def decimal_convert(n, b):
     """
@@ -19,19 +17,26 @@ def decimal_convert(n, b):
         This involves a while loop, mod, and div. 
         The function will end once x < 0.
     """
-    # Initialize an empty list to store the base n number representation
-    converted_num = []
+    # Initialize an empty string to store the base n number representation
+    converted_num = ''
 
     # Create a temporary variable to hold the changing value of n
     x = n
 
     # Create while loop
     while x > 0:
-        y = x % b   # Variable to store the converted digits into converted_num (x mod b = remainder)
-        converted_num.append(y)
+        y = str(x % b)   # Variable to store the converted digits into converted_num (x mod b = remainder)
+        converted_num += y
         x = x // b  # x will be updated for next iteration (x div b = quotient)
-    
-    return converted_num
+
+    # Flip string
+    converted_num = converted_num[::-1]
+
+    # Loop to remove leading zeros
+    while converted_num[0] == 0:
+        converted_num = converted_num[1::]
+
+    return int(converted_num)
 
 
 if __name__ == '__main__':
